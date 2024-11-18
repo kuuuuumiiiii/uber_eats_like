14f8545461f48d1de29de3e16f13f4891a46dae5
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2024-11-19 02:14:48 +0900

    6-3まで

---
+++ frontend/src/components/Icons/index.js
@@ -0,0 +1,3 @@
+export {default as LocalMallIcon} from '@mui/icons-material/LocalMall';
+export {default as QueryBuilderIcon} from '@mui/icons-material/QueryBuilder';
+
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,19 +1,113 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from '../reducers/foods';
+
 import { fetchFoods } from '../apis/foods';
 
-export const Foods = ({
-  match
-}) => {
+import { REQUEST_STATE } from '../constants';
+import { Link, useParams } from 'react-router-dom';
+
+import { styled } from 'styled-components'
+
+import { COLORS } from '../style_constants';
+import { LocalMallIcon } from '../components/Icons'; 
+
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@mui/material/Skeleton';
+
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
+
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
+
+export const Foods = () => {
+  const { restaurantsId } = useParams(); // URLパラメータを取得
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
   useEffect(() => {
-    fetchFoods(1)
-    .then((data) =>
-      console.log(data)
-    )
-  }, [])
+    dispatch({ type: foodsActionTyps.FETCHING });
+    fetchFoods(restaurantsId)
+      .then((data) => {
+        dispatch({
+          type: foodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods,
+          },
+        });
+      });
+  }, [restaurantsId]); // restaurantsIdを依存配列に追加
+
+
 
   return (
     <Fragment>
-      フード一覧
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+          :
+            foodsState.foodsList.map(food =>
+              <ItemWrapper key={food.id}>
+                <FoodWrapper
+                  food={food}
+                  onClickFoodWrapper={(food) => console.log(food)}
+                  imageUrl={FoodImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </FoodsList>
     </Fragment>
   );
 }
