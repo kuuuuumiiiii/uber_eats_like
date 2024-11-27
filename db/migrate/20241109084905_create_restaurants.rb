87365f05b3df5c06e3a37cc8b2352d77f29df4f3
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2024-11-27 17:09:16 +0900

    まとめと修正

--- db/migrate/20241109084905_create_restaurants.rb
+++ db/migrate/20241109084905_create_restaurants.rb
@@ -1,9 +1,9 @@
 class CreateRestaurants < ActiveRecord::Migration[7.1]
   def change
     create_table :restaurants do |t|
-      t.string :name, null: false
-      t.integer :fee, null: false, default: 0
-      t.integer :time_required, null: false
+      t.string :name, null: false, comment: 'レストランの名前'
+      t.integer :fee, null: false, default: 0, comment: '配送料'
+      t.integer :time_required, null: false, comment: '配達時間'
 
       t.timestamps
     end
--- db/migrate/20241109085115_create_line_foods.rb
+++ db/migrate/20241109085115_create_line_foods.rb
@@ -1,11 +1,11 @@
 class CreateLineFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :line_foods do |t|
-      t.references :food, null: false, foreign_key: true
-      t.references :restaurant, null: false, foreign_key: true
-      t.references :order, foreign_key: true
-      t.integer :count, null: false, default: 0
-      t.boolean :active, null: false, default: false
+      t.references :food, null: false, foreign_key: true, comment: 'フードの名前'
+      t.references :restaurant, null: false, foreign_key: true,comment: 'レストランの名前'
+      t.references :order, foreign_key: true, comment: '注文ID'
+      t.integer :count, null: false, default: 0, comment: '商品の数量'
+      t.boolean :active, null: false, default: false, comment: '論理削除できるようにする'
 
       t.timestamps
     end
